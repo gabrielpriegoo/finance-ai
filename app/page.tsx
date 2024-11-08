7955923bbc5beb1c37ff9f150b78a1ac53dc9c56
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-11-08 07:27:38 -0300

    chore: :lipstick: add shadcn ui component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1>
+        <Button>Link</Button>
+      </h1>
     </>
   );
 }
