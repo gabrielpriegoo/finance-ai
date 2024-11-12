62b77dd7731da24c06f2ff03811414bcb2556890
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-11-12 08:29:18 -0300

    feat: :sparkles: ensure transaction table labels are rendered properly

--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -10,22 +10,22 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   if (transaction.type === TransactionType.DEPOSIT) {
     return (
       <Badge className="bg-muted font-bold text-primary hover:bg-muted">
-        <CircleIcon className="mr-2 fill-primary" size={8} />
+        <CircleIcon className="mr-2 fill-primary" size={10} />
         Depósito
       </Badge>
     );
   }
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
-        <CircleIcon className="mr-2 fill-destructive" size={8} />
-        Despesas
+      <Badge className="font bold bg-danger bg-opacity-10 text-danger">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Despesa
       </Badge>
     );
   }
   return (
-    <Badge className="bg-white bg-opacity-10 font-bold text-white">
-      <CircleIcon className="mr-2 fill-white" size={8} />
+    <Badge className="font bold bg-white bg-opacity-10 text-white">
+      <CircleIcon className="mr-2 fill-white" size={10} />
       Investimento
     </Badge>
   );
