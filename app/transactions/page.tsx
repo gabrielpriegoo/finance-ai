a3c3056bde20d488a1c3cd85939886b2182cd1c1
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-11-22 08:08:45 -0300

    feat: :sparkles: add navbar component

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,4 +1,5 @@
 import AddTransactionButton from "../_components/add-transaction-button";
+import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactions_columns } from "./_columns";
@@ -7,14 +8,17 @@ const TransactionsPage = async () => {
   // acessar transações do banco de dados
   const transactions = await db.transaction.findMany({});
   return (
-    <div className="space-y-6 p-6">
-      {/* TITULO E BOTAO */}
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        {/* TITULO E BOTAO */}
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
+        <DataTable columns={transactions_columns} data={transactions} />
       </div>
-      <DataTable columns={transactions_columns} data={transactions} />
-    </div>
+    </>
   );
 };
 
