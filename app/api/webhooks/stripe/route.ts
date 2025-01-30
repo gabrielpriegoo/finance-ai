66b5b35f627c27f623b904d253b4332647030585
gabrielpriegoo <gabrielvpriego@gmail.com>
2025-01-30 09:10:43 -0300

    feat: :sparkles: add webhook stripe that handles subscritions

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,15 +2,15 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export const POST = async (req: Request) => {
+export const POST = async (request: Request) => {
   if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
     return NextResponse.error();
   }
-  const signature = req.headers.get("stripe-signature");
+  const signature = request.headers.get("stripe-signature");
   if (!signature) {
     return NextResponse.error();
   }
-  const text = await req.text();
+  const text = await request.text();
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-12-18.acacia",
   });
@@ -21,15 +21,17 @@ export const POST = async (req: Request) => {
   );
 
   switch (event.type) {
-    case "invoice.paid":
+    case "invoice.paid": {
+      // Atualizar o usuário com o seu novo plano
       const { customer, subscription, subscription_details } =
         event.data.object;
       const clerkUserId = subscription_details?.metadata?.clerk_user_id;
       if (!clerkUserId) {
         return NextResponse.error();
       }
-
-      (await clerkClient()).users.updateUser(clerkUserId, {
+      await (
+        await clerkClient()
+      ).users.updateUser(clerkUserId, {
         privateMetadata: {
           stripeCustomerId: customer,
           stripeSubscriptionId: subscription,
@@ -39,6 +41,28 @@ export const POST = async (req: Request) => {
         },
       });
       break;
+    }
+    case "customer.subscription.deleted": {
+      // Remover plano premium do usuário
+      const subscription = await stripe.subscriptions.retrieve(
+        event.data.object.id,
+      );
+      const clerkUserId = subscription.metadata.clerk_user_id;
+      if (!clerkUserId) {
+        return NextResponse.error();
+      }
+      await (
+        await clerkClient()
+      ).users.updateUser(clerkUserId, {
+        privateMetadata: {
+          stripeCustomerId: null,
+          stripeSubscriptionId: null,
+        },
+        publicMetadata: {
+          subscriptionPlan: null,
+        },
+      });
+    }
   }
   return NextResponse.json({ received: true });
 };
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,15 +1,19 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { CheckIcon, XIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import AcquirePlanButton from "./_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const Subscription = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
+  const user = (await clerkClient()).users.getUser(userId);
+  const hasPremiumPlan =
+    (await user).publicMetadata.subscriptionPlan === "premium";
   return (
     <>
       <Navbar />
@@ -41,7 +45,12 @@ const Subscription = async () => {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute right-4 top-4 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
